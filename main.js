3f8938028efe4c07ccd6261a0e20609c00106155
y-2j <etozhedafomenko@gmail.com>
2019-09-10 11:15:33 +0400

    Выполнение заданий

---
+++ main.js
@@ -0,0 +1,25 @@
+//Задание 1
+
+let algebra = 5;
+let geography = 4;
+let physics = 4; 
+let mean = (algebra + geography + physics) / 3;
+
+console.log(mean);
+
+
+console.log();
+//Задание 2 
+let name = 'Степан';
+let message = `Привет, мир! Меня зовут ${name}`; 
+
+console.log(message);
+
+console.log();
+//Задание 3
+let x = 2;
+let y = 22;
+let z =  0; 
+let result = x * y + 5 * z + x - 1; 
+
+console.log(result);
